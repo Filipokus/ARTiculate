9776ca5c7f56bbc066c8cb0ad19b609d236242b2
Jonathan <joty1901@users.noreply.github.com>
2021-01-27 14:33:37 +0100

    Add project files.

---
+++ ARTiculateDataAccessLibrary/Models/Test.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ARTiculateDataAccessLibrary.Models
+{
+    class Test
+    {
+        //Snälla gud fungera!
+    }
+}
