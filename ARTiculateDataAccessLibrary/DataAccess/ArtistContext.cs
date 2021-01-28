68695a64b9c8325b432f75795ffde09a3ad8bcdb
Jonathan <joty1901@users.noreply.github.com>
2021-01-28 08:59:16 +0100

    - DbSet for studio in ArtistContext class

--- ARTiculateDataAccessLibrary/DataAccess/ArtistContext.cs
+++ ARTiculateDataAccessLibrary/DataAccess/ArtistContext.cs
@@ -9,12 +9,13 @@ namespace ARTiculateDataAccessLibrary.DataAccess
     class ArtistContext : DbContext
     {
         public ArtistContext(DbContextOptions options) : base(options) { }
+
         public DbSet<Artist> Artists { get; set; }
-        public DbSet<Tag> Tags { get; set; }
         public DbSet<ArtItem> ArtItems { get; set; }
-        public DbSet<Vernisage> Vernisages { get; set; }
         public DbSet<Exhibition> Exhibitions { get; set; }
-        public DbSet<Studio> Studio { get; set; }
         public DbSet<Link> Link { get; set; }
+        public DbSet<Studio> Studio { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Vernisage> Vernisages { get; set; }
     }
 }
