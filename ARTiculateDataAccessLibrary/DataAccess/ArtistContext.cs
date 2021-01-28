48e637b42157e7aeb03bca76c3745a143aabf32d
Jonathan <joty1901@users.noreply.github.com>
2021-01-28 11:09:42 +0100

    - Created method OnModelCreating for combined tables to configure composite keys

--- ARTiculateDataAccessLibrary/DataAccess/ArtistContext.cs
+++ ARTiculateDataAccessLibrary/DataAccess/ArtistContext.cs
@@ -22,5 +22,31 @@ namespace ARTiculateDataAccessLibrary.DataAccess
         public DbSet<ArtItem_Tag> ArtItem_Tags { get; set; }
         public DbSet<Artist_Tag> Artist_Tags { get; set; }
         public DbSet<Studio_Tag> Studio_Tags { get; set; }
+        public DbSet<Artist_Exhibition> Artist_Exhibitions { get; set; }
+        public DbSet<Artist_Vernisage> Artist_Vernisages { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Artist_Exhibition>()
+                .HasKey(ae => new { ae.ArtistId, ae.ExhibitionId });
+
+            modelBuilder.Entity<Artist_Tag>()
+              .HasKey(at => new { at.ArtistId, at.TagId });
+
+            modelBuilder.Entity<Artist_Vernisage>()
+                .HasKey(av => new { av.ArtistId, av.VernisageId });
+
+            modelBuilder.Entity<ArtItem_Tag>()
+                .HasKey(ait => new { ait.ArtItemId, ait.TagId });
+
+            modelBuilder.Entity<Exhibition_Tag>()
+                .HasKey(et => new { et.ExhibitionId, et.TagId });
+
+            modelBuilder.Entity<Studio_Tag>()
+                .HasKey(st => new { st.StudioId, st.TagId });
+
+            modelBuilder.Entity<Vernisage_Tag>()
+               .HasKey(vt => new { vt.VernisageId, vt.TagId });
+        }
     }
 }
