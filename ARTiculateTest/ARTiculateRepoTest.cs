ad72cf22134d5e07e2a357262ee2cb95516dd4af
Jonathan <joty1901@users.noreply.github.com>
2021-02-02 13:37:46 +0100

    - Added referens between ARTiculateTest and Webproject - Added test class (ARTiculateRepoTest) in ARTiculateTest

---
+++ ARTiculateTest/ARTiculateRepoTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ARTiculateTest
+{
+    public class ARTiculateRepoTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+
+    }
+}
