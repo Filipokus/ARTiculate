af046efe3dd6c709330c36c82138b6b10986af91
Jonathan <joty1901@users.noreply.github.com>
2021-02-08 11:09:26 +0100

    - Added props ExhibitionId and object Exhibition to class Vernisage in DataAccessLibrary - Added migration VernisageToExhibtionConstraint where exhibitionId is FK in table Vernisages and is nullable.

---
+++ ARTiculateDataAccessLibrary/Migrations/20210208100154_VernisageToExhibitionConstraint.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ARTiculateDataAccessLibrary.Migrations
+{
+    public partial class VernisageToExhibitionConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ExhibitionId",
+                table: "Vernisages",
+                nullable: true,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vernisages_ExhibitionId",
+                table: "Vernisages",
+                column: "ExhibitionId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Vernisages_Exhibitions_ExhibitionId",
+                table: "Vernisages",
+                column: "ExhibitionId",
+                principalTable: "Exhibitions",
+                principalColumn: "Id");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Vernisages_Exhibitions_ExhibitionId",
+                table: "Vernisages");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Vernisages_ExhibitionId",
+                table: "Vernisages");
+
+            migrationBuilder.DropColumn(
+                name: "ExhibitionId",
+                table: "Vernisages");
+        }
+    }
+}
