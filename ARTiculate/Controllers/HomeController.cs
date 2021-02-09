9407ed4cfcbf37f8185d36173135b5eb897ddfdc
Gustav <Gustav@DESKTOP-PTNUM16>
2021-02-09 12:04:09 +0100

    Created log in and Log out function. Magnus, Gustav och Florim.

--- ARTiculate/Controllers/HomeController.cs
+++ ARTiculate/Controllers/HomeController.cs
@@ -11,6 +11,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using ARTiculate.Mock;
 using ARTiculate.Data;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using ARTiculate.Areas.Identity.Data;
 
 namespace ARTiculate.Controllers
 {
@@ -19,13 +22,15 @@ namespace ARTiculate.Controllers
         private readonly ILogger<HomeController> _logger;
         private readonly ArtistContext _db;
         private IARTiculateRepository aRTiculateRepository;
+        private readonly SignInManager<ARTiculateUser> _signInManager;
 
 
-        public HomeController(ILogger<HomeController> logger, ArtistContext db, IARTiculateRepository aRTiculateRepository)
+        public HomeController(ILogger<HomeController> logger, ArtistContext db, IARTiculateRepository aRTiculateRepository, SignInManager<ARTiculateUser> signInManager)
         {
             _logger = logger;
             _db = db;
             this.aRTiculateRepository = aRTiculateRepository;
+            _signInManager = signInManager;
         }
 
         public async Task<IActionResult> Index()
@@ -48,6 +53,17 @@ namespace ARTiculate.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<IActionResult> LogOut()
+        {
+            await _signInManager.SignOutAsync();
+
+            //Session.Clear();
+
+
+
+            return RedirectToAction("Index");
         }
     }
 }
