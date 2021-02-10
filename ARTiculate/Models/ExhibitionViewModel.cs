3feb3692d01dcb93d27c7c6df0c21050de143259
Jonathan <joty1901@users.noreply.github.com>
2021-02-10 09:11:14 +0100

    - Removed ApiClient and IApiClient since it were never used. - Added testcase in ExhibitionViewModelTest

--- ARTiculate/Models/ExhibitionViewModel.cs
+++ ARTiculate/Models/ExhibitionViewModel.cs
@@ -9,16 +9,52 @@ namespace ARTiculate.Models
     public class ExhibitionViewModel
     {
         public Exhibition Exhibition { get; set; }
-        public List<Artist> Artists { get; set; } = new List<Artist>();
-
-        public ExhibitionViewModel(Exhibition exhibition)
-        {
-            this.Exhibition = exhibition;
-        }
+        public List<Exhibition> Exhibitions { get; set; } = new List<Exhibition>();
+        public List<Exhibition> NewlyAddedExhibitions { get; set; } = new List<Exhibition>();
+        public List<Exhibition> ExhibitionsByTagName { get; set; } = new List<Exhibition>();
 
         public ExhibitionViewModel()
         {
 
+        }
+
+        public ExhibitionViewModel(Exhibition exhibition)
+        {
+            Exhibition = exhibition;
+        }
+
+        public ExhibitionViewModel(List<Exhibition> exhibitions)
+        {
+            Exhibitions = exhibitions;
+            NewlyAddedAxhibitions();
+           // ExhibitionsByTagName = SortExhibitionsByTagName("rymden");
+        }
+
+        private void NewlyAddedAxhibitions()
+        {
+            for (int i = 0; (i < 6) && (i < Exhibitions.Count); i++)
+            {
+                NewlyAddedExhibitions.Add(Exhibitions[i]);
+            }
+        }
+
+        public List<Exhibition> SortExhibitionsByTagName(string tagName, List<Exhibition> testList)
+        {
+            List<Exhibition> list = new List<Exhibition>();
+
+            foreach (var exhibition in testList)
+            {
+                foreach (var tag in exhibition.Exhibition_Tags)
+                {
+                    if (tagName.Equals(tag.Tag.TagName))
+                    {
+                        list.Add(exhibition);
+                        break;
+                    }
+                }
+            }
+
+            return list;
         }
     }
 }
--- ARTiculateTest/ExhibitionViewModelTest.cs
+++ ARTiculateTest/ExhibitionViewModelTest.cs
@@ -11,54 +11,138 @@ namespace ARTiculateTest
     {
         ExhibitionViewModel viewModel = new ExhibitionViewModel();
 
+
         [Fact]
-        public void GetArtistsForSelectedExhibition_ShouldRetrunListOfArtists()
+        public void SortExhibitionsByTagName_ShouldReturnExhibitionsByTag()
         {
             //Arrange
-            Artist artist = new Artist
+            string tagName = "rymden";
+
+            Tag tagRymden = new Tag
             {
-                Id = 1,
-                Firstname = "Test",
-                Lastname = "Testsson"
+                TagName = "rymden"
             };
 
-            Artist artistTwo = new Artist
+            Tag tagSpace = new Tag
             {
-                Id = 2,
-                Firstname = "Jest",
-                Lastname = "Jestsson"
+                TagName = "space"
             };
 
-            Artist_Exhibition artist_Exhibition = new Artist_Exhibition
+            Tag tagSkog = new Tag
             {
-                Artist = artist
+                TagName = "skog"
             };
 
-            Artist_Exhibition artist_ExhibitionTwo = new Artist_Exhibition
+            Tag tagPaint = new Tag
             {
-                Artist = artistTwo
+                TagName = "paint"
             };
 
-            List<Artist_Exhibition> artist_Exhibitions = new List<Artist_Exhibition>();
-            artist_Exhibitions.Add(artist_Exhibition);
-            artist_Exhibitions.Add(artist_ExhibitionTwo);
+            Exhibition_Tag exhibition_tagRymden = new Exhibition_Tag();
+            Exhibition_Tag exhibition_tagSpace = new Exhibition_Tag();
+            Exhibition_Tag exhibition_tagSkog = new Exhibition_Tag();
+            Exhibition_Tag exhibition_tagPaint = new Exhibition_Tag();
 
-            Exhibition exhibition = new Exhibition
-            {
-                Artist_Exhibitions = artist_Exhibitions
+            exhibition_tagRymden.Tag = tagRymden;
+            exhibition_tagSpace.Tag = tagSpace;
+            exhibition_tagSkog.Tag = tagSkog;
+            exhibition_tagPaint.Tag = tagPaint;
+
+            Exhibition exhibitionRymdenSpace = new Exhibition
+            { 
+                Title = "Rymden And Space"
             };
 
-            List<Artist> expected = new List<Artist>();
-            expected.Add(artist);
-            expected.Add(artistTwo);
+            Exhibition exhibitionSkogPaint = new Exhibition
+            {
+                Title = "Skog And Paint"
+            };
+
+            Exhibition exhibitionAllTags = new Exhibition
+            {
+                Title = "All Test Tags"
+            };
+
+            //Rymden and Space
+            exhibitionRymdenSpace.Exhibition_Tags.Add(exhibition_tagRymden);
+            exhibitionRymdenSpace.Exhibition_Tags.Add(exhibition_tagSpace);
+
+            //Skog and paint
+            exhibitionSkogPaint.Exhibition_Tags.Add(exhibition_tagSkog);
+            exhibitionSkogPaint.Exhibition_Tags.Add(exhibition_tagPaint);
+
+            //All test tags
+            exhibitionAllTags.Exhibition_Tags.Add(exhibition_tagRymden);
+            exhibitionAllTags.Exhibition_Tags.Add(exhibition_tagSpace);
+            exhibitionAllTags.Exhibition_Tags.Add(exhibition_tagSkog);
+            exhibitionAllTags.Exhibition_Tags.Add(exhibition_tagPaint);
+
+            //Adds all exhibitions above in list och exhibitions
+            List<Exhibition> listofAllTestExhibitions = new List<Exhibition>();
+            listofAllTestExhibitions.Add(exhibitionRymdenSpace);
+            listofAllTestExhibitions.Add(exhibitionSkogPaint);
+            listofAllTestExhibitions.Add(exhibitionAllTags);
+
+            //The result should only contain exhibitionRymdenSpace and exhibitionAllTags
+            List<Exhibition> expected = new List<Exhibition>();
+            expected.Add(exhibitionRymdenSpace);
+            expected.Add(exhibitionAllTags);
 
             //Act
-            List<Artist> actual = viewModel.GetArtistsForSelectedExhibition(exhibition);
+            var actual = viewModel.SortExhibitionsByTagName(tagName, listofAllTestExhibitions);
 
             //Assert
-            Assert.True(actual != null);
             Assert.Equal(expected, actual);
-
         }
+
+        //[Fact]
+        //public void GetArtistsForSelectedExhibition_ShouldRetrunListOfArtists()
+        //{
+        //    //Arrange
+        //    Artist artist = new Artist
+        //    {
+        //        Id = 1,
+        //        Firstname = "Test",
+        //        Lastname = "Testsson"
+        //    };
+
+        //    Artist artistTwo = new Artist
+        //    {
+        //        Id = 2,
+        //        Firstname = "Jest",
+        //        Lastname = "Jestsson"
+        //    };
+
+        //    Artist_Exhibition artist_Exhibition = new Artist_Exhibition
+        //    {
+        //        Artist = artist
+        //    };
+
+        //    Artist_Exhibition artist_ExhibitionTwo = new Artist_Exhibition
+        //    {
+        //        Artist = artistTwo
+        //    };
+
+        //    List<Artist_Exhibition> artist_Exhibitions = new List<Artist_Exhibition>();
+        //    artist_Exhibitions.Add(artist_Exhibition);
+        //    artist_Exhibitions.Add(artist_ExhibitionTwo);
+
+        //    Exhibition exhibition = new Exhibition
+        //    {
+        //        Artist_Exhibitions = artist_Exhibitions
+        //    };
+
+        //    List<Artist> expected = new List<Artist>();
+        //    expected.Add(artist);
+        //    expected.Add(artistTwo);
+
+        //    //Act
+        //    List<Artist> actual = viewModel.GetArtistsForSelectedExhibition(exhibition);
+
+        //    //Assert
+        //    Assert.True(actual != null);
+        //    Assert.Equal(expected, actual);
+
+        //}
     }
 }
