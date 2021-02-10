3d808f48edfc702d64c1c78564c9797566c34db7
Jonathan <joty1901@users.noreply.github.com>
2021-02-10 16:31:51 +0100

    - Created test in ExhibitionViewModelTest for method FourPosters - Created method FourPosters -Created methd CheckIfFourPosters

--- ARTiculate/Models/ExhibitionViewModel.cs
+++ ARTiculate/Models/ExhibitionViewModel.cs
@@ -10,7 +10,7 @@ namespace ARTiculate.Models
     {
         public Exhibition Exhibition { get; set; }
         public List<Exhibition> Exhibitions { get; set; } = new List<Exhibition>();
-        public List<Exhibition> NewlyAddedExhibitions { get; set; } = new List<Exhibition>();
+        public List<Exhibition> NewlyAddedExhibitions { get; set; }
         public List<Exhibition> ExhibitionsByTagName { get; set; }
         public string TagName { get; set; }
 
@@ -25,23 +25,89 @@ namespace ARTiculate.Models
             Exhibition = exhibition;
         }
 
+        //public ExhibitionViewModel(List<Exhibition> exhibitions)
+        //{
+        //    Exhibitions = exhibitions;
+        //    GetNewlyAddedExhibitions();
+        //    ExhibitionsByTagName = SortExhibitionsByTagName(TagName, exhibitions);
+        //}
+
         public ExhibitionViewModel(List<Exhibition> exhibitions)
         {
             Exhibitions = exhibitions;
-            GetNewlyAddedExhibitions();
+            NewlyAddedExhibitions = GetNewlyAddedExhibitionsForView(exhibitions);
             ExhibitionsByTagName = SortExhibitionsByTagName(TagName, exhibitions);
         }
+
+        public List<Exhibition> GetNewlyAddedExhibitionsForView(List<Exhibition> exhibitions)
+        {
+            List<Exhibition> listOfNewlyAddedExhibitions = new List<Exhibition>();
+
+            for (int i = 0; (i < 6) && (i < exhibitions.Count); i++)
+            {
+                listOfNewlyAddedExhibitions.Add(exhibitions[i]);
+            }
+
+            if (CheckIfFourPosters(listOfNewlyAddedExhibitions))
+            {
+                return listOfNewlyAddedExhibitions;
+            }
+            else
+            {
+                var list = FourPosters(listOfNewlyAddedExhibitions);
+                return list;
+            }
+
+        }
+
+        public List<Exhibition> FourPosters(List<Exhibition> exhibitions)
+        {
+            List<Exhibition> exhibitionsWithFourPosters = new List<Exhibition>();
+
+            foreach (var exhibition in exhibitions)
+            {
+                var newExhibition = exhibition;
+                List<Exhibition_ArtItem> listOfExhibitionsWithFourArtItems = new List<Exhibition_ArtItem>();
+
+                if (exhibition.Exhibition_ArtItem.Count >= 4)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        listOfExhibitionsWithFourArtItems.Add(exhibition.Exhibition_ArtItem[i]);
+                    }
+                    newExhibition.Exhibition_ArtItem = listOfExhibitionsWithFourArtItems;
+                    exhibitionsWithFourPosters.Add(newExhibition);
+                }
+            }
+
+            return exhibitionsWithFourPosters;
+        }
+
+
+        public bool CheckIfFourPosters(List<Exhibition> exhibitions)
+        {
+            foreach (var exhibition in exhibitions)
+            {
+                if (exhibition.Exhibition_ArtItem.Count != 4)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
 
         /// <summary>
         /// Updates the list NewlyAddedExhibitions with the 6 most recent added Exhibitions.
         /// </summary>
-        private void GetNewlyAddedExhibitions()
-        {
-            for (int i = 0; (i < 6) && (i < Exhibitions.Count); i++)
-            {
-                NewlyAddedExhibitions.Add(Exhibitions[i]);
-            }
-        }
+        //public void GetNewlyAddedExhibitions()
+        //{
+        //    for (int i = 0; (i < 6) && (i < Exhibitions.Count); i++)
+        //    {
+        //        NewlyAddedExhibitions.Add(Exhibitions[i]);
+        //    }
+        //}
 
         /// <summary>
         /// Creates a new list with all Exhibitions that is tagged with the specified name of the tag
--- ARTiculateTest/ExhibitionViewModelTest.cs
+++ ARTiculateTest/ExhibitionViewModelTest.cs
@@ -1,8 +1,11 @@
-﻿using ARTiculate.Models;
+﻿using ARTiculate.Data;
+using ARTiculate.Models;
 using ARTiculateDataAccessLibrary.Models;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace ARTiculateTest
@@ -10,6 +13,99 @@ namespace ARTiculateTest
     public class ExhibitionViewModelTest
     {
         ExhibitionViewModel viewModel = new ExhibitionViewModel();
+
+        private readonly List<Exhibition> listofAllTestExhibitions;
+
+        public ExhibitionViewModelTest()
+        {
+            listofAllTestExhibitions = MockTestData();
+        }
+
+        private List<Exhibition> MockTestData()
+        {
+            //Arrange
+            string tagName = "rymden";
+
+            Tag tagRymden = new Tag { TagName = "rymden" };
+            Tag tagSpace = new Tag { TagName = "space" };
+            Tag tagSkog = new Tag { TagName = "skog" };
+            Tag tagPaint = new Tag { TagName = "paint" };
+
+            ArtItem artItemBowl = new ArtItem { Name = "Mystic" };
+            ArtItem artItemTree = new ArtItem { Name = "Tree" };
+            ArtItem artItemStone = new ArtItem { Name = "Stone" };
+            ArtItem artItemFace = new ArtItem { Name = "Face" };
+
+            Exhibition_ArtItem exhibition_ArtItemBowl = new Exhibition_ArtItem { ArtItems = artItemBowl };
+            Exhibition_ArtItem exhibition_ArtItemTree = new Exhibition_ArtItem { ArtItems = artItemTree };
+            Exhibition_ArtItem exhibition_ArtItemStone = new Exhibition_ArtItem { ArtItems = artItemStone };
+            Exhibition_ArtItem exhibition_ArtItemFace = new Exhibition_ArtItem { ArtItems = artItemFace };
+
+            Exhibition_Tag exhibition_tagRymden = new Exhibition_Tag();
+            Exhibition_Tag exhibition_tagSpace = new Exhibition_Tag();
+            Exhibition_Tag exhibition_tagSkog = new Exhibition_Tag();
+            Exhibition_Tag exhibition_tagPaint = new Exhibition_Tag();
+
+            exhibition_tagRymden.Tag = tagRymden;
+            exhibition_tagSpace.Tag = tagSpace;
+            exhibition_tagSkog.Tag = tagSkog;
+            exhibition_tagPaint.Tag = tagPaint;
+
+            Exhibition exhibitionRymdenSpace = new Exhibition { Title = "Rymden And Space" };
+            Exhibition exhibitionSkogPaint = new Exhibition { Title = "Skog And Paint" };
+            Exhibition exhibitionAllTags = new Exhibition { Title = "All Test Tags" };
+
+            //Rymden and Space
+            exhibitionRymdenSpace.Exhibition_Tags.Add(exhibition_tagRymden);
+            exhibitionRymdenSpace.Exhibition_Tags.Add(exhibition_tagSpace);
+
+            exhibitionRymdenSpace.Exhibition_ArtItem.Add(exhibition_ArtItemBowl);
+            exhibitionRymdenSpace.Exhibition_ArtItem.Add(exhibition_ArtItemTree);
+            exhibitionRymdenSpace.Exhibition_ArtItem.Add(exhibition_ArtItemStone);
+            exhibitionRymdenSpace.Exhibition_ArtItem.Add(exhibition_ArtItemFace);
+
+            //Skog and paint
+            exhibitionSkogPaint.Exhibition_Tags.Add(exhibition_tagSkog);
+            exhibitionSkogPaint.Exhibition_Tags.Add(exhibition_tagPaint);
+
+            //All test tags
+            exhibitionAllTags.Exhibition_Tags.Add(exhibition_tagRymden);
+            exhibitionAllTags.Exhibition_Tags.Add(exhibition_tagSpace);
+            exhibitionAllTags.Exhibition_Tags.Add(exhibition_tagSkog);
+            exhibitionAllTags.Exhibition_Tags.Add(exhibition_tagPaint);
+
+            //Adds all exhibitions above in list och exhibitions
+            List<Exhibition> listofAllTestExhibitions = new List<Exhibition>();
+            listofAllTestExhibitions.Add(exhibitionRymdenSpace);
+            listofAllTestExhibitions.Add(exhibitionSkogPaint);
+            listofAllTestExhibitions.Add(exhibitionAllTags);
+
+            return listofAllTestExhibitions;
+        }
+
+        [Fact]
+        public void FourPosters_ShouldReturnExhibitionsWithOnlyFourPosters()
+        {
+            //Arrange
+            List<Exhibition> test = MockTestData();
+
+            List<Exhibition> expected = new List<Exhibition>();
+            expected.Add(listofAllTestExhibitions[0]);
+
+            ArtItem artItemStoneFace = new ArtItem { Name = "StoneFace" };
+            Exhibition_ArtItem exhibition_ArtItemStoneFace = new Exhibition_ArtItem { ArtItems = artItemStoneFace };
+
+            test[0].Exhibition_ArtItem.Add(exhibition_ArtItemStoneFace);
+
+            //Act
+            List<Exhibition> actual = viewModel.FourPosters(test);
+
+            //Assert
+            Assert.Equal(expected, actual);
+            Assert.True(actual[0].Exhibition_ArtItem.Count == expected[0].Exhibition_ArtItem.Count);
+
+            
+        }
 
 
         [Fact]
@@ -67,6 +163,27 @@ namespace ARTiculateTest
 
             //Assert
             Assert.Equal(expected, actual);
+            Assert.True(actual.Count >= 1);
+            Assert.Contains<Exhibition>(exhibitionAllTags, expected);
+        }
+
+        public List<Exhibition> Exhibitions { get; set; } = new List<Exhibition>();
+        public List<Exhibition> NewlyAddedExhibitions { get; set; } = new List<Exhibition>();
+
+        [Fact]
+        public async void GetNewlyAddedExhibitions_ShouldWork()
+        {
+            Exhibition exhibition = new Exhibition { Title = "Test1" };
+            var exhibitions = listofAllTestExhibitions;
+
+            var mock = new Mock<IARTiculateRepository>();
+            var sut = new ARTiculateRepositoryMock("test");
+
+            mock.Setup(x => x.GetExhibitionsFromDbOrderedByDate()).Returns(Task.FromResult(exhibitions));
+            var actual = await sut.GetExhibitionsFromDbOrderedByDate();
+
+            //viewModel.GetNewlyAddedExhibitions();
+
         }
 
         //[Fact]
