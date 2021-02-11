a172d03a52da18b2df97a384d80895d48a3f62ff
Pilvii <pilviborjesson@gmail.com>
2021-02-11 15:00:34 +0100

    Continue with studio page "folders". Evelina and Pilvi worked together. TrelloID: 16.2

--- ARTiculate/wwwroot/Assets/js/studio.js
+++ ARTiculate/wwwroot/Assets/js/studio.js
@@ -1,10 +1,21 @@
-﻿function openFolder(ev) {
-    let folderContainer = ev.parentNode.parentNode
-    let folder = folderContainer.children[0]
-    let arrow = folderContainer.children[1]
-    let folderItems = folderContainer.children[2]
-    console.dir(folderContainer)
+﻿
 
-    ////Lägger på klass med margins
-    //folder.classList.Add()
+function toggleOpenFolder(ev) {
+
+    let folder = ev.parentNode
+    let arrow = folder.nextElementSibling.children[0]
+    let folderItems = folder.nextElementSibling.children[1]
+
+    if (folder.classList.contains('folder-open')) {
+        //Close folder
+        folder.classList.remove('folder-open')
+        folderItems.classList.remove('box-open')
+        arrow.classList.remove('arrow-visible')
+    }
+    else {
+        //Open folder
+        folder.classList.add('folder-open')
+        folderItems.classList.add('box-open')
+        arrow.classList.add('arrow-visible')
+    }
 }
