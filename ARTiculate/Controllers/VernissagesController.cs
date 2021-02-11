e401e6453eda853245d6b2362b8a64d45f8f4ca8
Olivia Björner <filipokus.130@hotmail.com>
2021-02-11 12:42:33 +0100

    Further Merge of 'Develop' into 'Back-end'

--- ARTiculate/Controllers/VernissagesController.cs
+++ ARTiculate/Controllers/VernissagesController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using ARTiculate.Data;
 using ARTiculateDataAccessLibrary.Models;
 using ARTiculate.Models;
+using Microsoft.AspNetCore.Identity;
+using ARTiculate.Areas.Identity.Data;
 
 namespace ARTiculate.Controllers
 {
@@ -13,10 +15,14 @@ namespace ARTiculate.Controllers
     {
         
         private IARTiculateRepository ARTiculateRepository;
+        private UserManager<ARTiculateUser> userManager;
 
-        public VernissagesController(IARTiculateRepository ARTiculateRepository)
+        public VernissagesController(
+            IARTiculateRepository ARTiculateRepository,
+            UserManager<ARTiculateUser> userManager)
         {
             this.ARTiculateRepository = ARTiculateRepository;
+            this.userManager = userManager;
         }
 
         public async Task<IActionResult> Index()
@@ -31,24 +37,33 @@ namespace ARTiculate.Controllers
 
         public async Task<IActionResult> Vernissage(int ID)
         {
-            var viewModel = await GetVernisageViewModel(ID);
+            Vernisage vernisage = await ARTiculateRepository.GetVernisage(ID);
+            VernisageViewModel viewModel;
+
+            if (User.Identity.IsAuthenticated)
+            {
+                ARTiculateUser user = await GetCurrentUserAsync();
+                Artist artist = new Artist();
+                artist = ARTiculateRepository.CreateArtistFromARTiculateUser(user);
+                viewModel = new VernisageViewModel(vernisage, artist);
+            }
+            else
+            {
+                viewModel = new VernisageViewModel(vernisage);
+            }
 
             return View(viewModel);
         }
+
+        private Task<ARTiculateUser> GetCurrentUserAsync() => userManager.GetUserAsync(HttpContext.User);
+
 
         public async Task<IActionResult> About(int ID)
         {
-            var viewModel = await GetVernisageViewModel(ID);
+            Vernisage vernisage = await ARTiculateRepository.GetVernisage(ID);
+            AboutVernisageViewModel viewModel = new AboutVernisageViewModel(vernisage);
 
             return View(viewModel);
-        }
-
-        private async Task<VernisageViewModel> GetVernisageViewModel(int id)
-        {
-            Vernisage vernisage = await ARTiculateRepository.GetVernisage(id);
-            VernisageViewModel viewModel = new VernisageViewModel(vernisage);
-
-            return viewModel;
         }
     }
 }
