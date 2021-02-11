3e49e4d786db53a62da8a2904e79b4ca69e35086
Pilvii <pilviborjesson@gmail.com>
2021-02-11 12:24:51 +0100

    Continue working on studion admin page. Evelina and Pilvi worked together. TrelloID: 16.2

---
+++ ARTiculate/wwwroot/Assets/js/studio.js
@@ -0,0 +1,10 @@
+﻿function openFolder(ev) {
+    let folderContainer = ev.parentNode.parentNode
+    let folder = folderContainer.children[0]
+    let arrow = folderContainer.children[1]
+    let folderItems = folderContainer.children[2]
+    console.dir(folderContainer)
+
+    ////Lägger på klass med margins
+    //folder.classList.Add()
+}
