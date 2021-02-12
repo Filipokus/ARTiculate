8d6e4cc44a3e1038fd35acbf82321ab17d66a472
Pilvii <pilviborjesson@gmail.com>
2021-02-12 13:52:05 +0100

    Fixed js for site.js

--- ARTiculate/wwwroot/Assets/js/site.js
+++ ARTiculate/wwwroot/Assets/js/site.js
@@ -90,7 +90,7 @@ function loadNewContent(arrowBtn) {
     }
 
     else {
-        if (carouselDivNumber !== numberOfChildren-1) {
+        if (carouselDivNumber !== numberOfChildren - 1) {
             childrenOfStraightDiv[carouselDivNumber + 1].style.display = 'initial';
         }
         else {
@@ -98,55 +98,55 @@ function loadNewContent(arrowBtn) {
         }
         activeCarouselDiv.style.display = 'none';
     }
-
-//---------Close up of image---------
-
-function viewImageCloseUp(ev) {
-    //skapar kopia av bilden
-    let image = document.createElement("img");
-    image.src = ev.src;
-
-    let veil = document.getElementById("veil");
-    let imagebox = document.getElementById("imageCloseUp-container");
-    let artInfo = document.getElementById("artworkInfo");
-
-    //lägger in bilden i veil
-    image.classList.add('shownImage');
-    imagebox.prepend(image);
-
-    //skapar stäng-knapp
-    let close = document.createElement("img");
-    close.src = "/Assets/Images/close.png";
-    close.setAttribute("onclick", "hideImageCloseUp();");
-    close.classList.add('close');
-    imagebox.prepend(close);
-
-    //Visar veil
-    veil.classList.remove('veilHidden');
-    veil.classList.add('veilShowing');
-
-    //Visar info
-    artInfo.classList.remove('hideInfo');
-    artInfo.classList.add('showInfo');
 }
+    //---------Close up of image---------
 
-function hideImageCloseUp() {
-    let veil = document.getElementById("veil");
-    let imagebox = document.getElementById("imageCloseUp-container");
-    let artInfo = document.getElementById("artworkInfo");
+    function viewImageCloseUp(ev) {
+        //skapar kopia av bilden
+        let image = document.createElement("img");
+        image.src = ev.src;
 
-    //Tar bort bilden
-    imagebox.removeChild(imagebox.childNodes[0]);
-    imagebox.removeChild(imagebox.childNodes[0]);
+        let veil = document.getElementById("veil");
+        let imagebox = document.getElementById("imageCloseUp-container");
+        let artInfo = document.getElementById("artworkInfo");
 
-    //Gömmer veil
-    veil.classList.remove('veilShowing');
-    veil.classList.add('veilHidden');
+        //lägger in bilden i veil
+        image.classList.add('shownImage');
+        imagebox.prepend(image);
 
-    //Gömmer Info
-    artInfo.classList.remove('showInfo');
-    artInfo.classList.add('hideInfo');
-}
+        //skapar stäng-knapp
+        let close = document.createElement("img");
+        close.src = "/Assets/Images/close.png";
+        close.setAttribute("onclick", "hideImageCloseUp();");
+        close.classList.add('close');
+        imagebox.prepend(close);
+
+        //Visar veil
+        veil.classList.remove('veilHidden');
+        veil.classList.add('veilShowing');
+
+        //Visar info
+        artInfo.classList.remove('hideInfo');
+        artInfo.classList.add('showInfo');
+    }
+
+    function hideImageCloseUp() {
+        let veil = document.getElementById("veil");
+        let imagebox = document.getElementById("imageCloseUp-container");
+        let artInfo = document.getElementById("artworkInfo");
+
+        //Tar bort bilden
+        imagebox.removeChild(imagebox.childNodes[0]);
+        imagebox.removeChild(imagebox.childNodes[0]);
+
+        //Gömmer veil
+        veil.classList.remove('veilShowing');
+        veil.classList.add('veilHidden');
+
+        //Gömmer Info
+        artInfo.classList.remove('showInfo');
+        artInfo.classList.add('hideInfo');
+    }
 
 
 
